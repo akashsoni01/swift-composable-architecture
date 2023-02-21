a6b70ba496ba2580f9dae130d438da2d65ab848e
Akash Soni <akashsoni8285@gmail.com>
2023-02-21 19:11:03 +0530

    initial screen setup

--- Examples/Inventory/Inventory/InventoryView.swift
+++ Examples/Inventory/Inventory/InventoryView.swift
@@ -9,22 +9,87 @@ import SwiftUI
 import ComposableArchitecture
 
 struct InventoryFeature: Reducer {
-    struct State : Equatable {}
-    enum Action: Equatable {}
+    struct State : Equatable {
+        var items: IdentifiedArrayOf<Item> = []
+        
+    }
+    enum Action: Equatable {
+        case deleteButtonTapped(id: Item.ID)
+    }
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
-        return .none
+        switch action {
+        case .deleteButtonTapped(id: let id):
+            // Show alert
+            return .none
+        }
     }
 }
 
 struct InventoryView: View {
     let store: StoreOf<InventoryFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(
+          self.store, observe: \.items
+        ) { viewStore in
+          List {
+            ForEach(viewStore.state) { item in
+              HStack {
+                VStack(alignment: .leading) {
+                  Text(item.name)
+
+                  switch item.status {
+                  case let .inStock(quantity):
+                    Text("In stock: \(quantity)")
+                  case let .outOfStock(isOnBackOrder):
+                      Text(
+                        "Out of stock \(isOnBackOrder ? ": on back order" : "")"
+                      )
+                  }
+                }
+
+                Spacer()
+
+                if let color = item.color {
+                  Rectangle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(color.swiftUIColor)
+                    .border(Color.black, width: 1)
+                }
+
+                Button {
+                    viewStore.send(.deleteButtonTapped(id: item.id))
+                } label: {
+                  Image(systemName: "trash.fill")
+                }
+                .padding(.leading)
+              }
+              .buttonStyle(.plain)
+              .foregroundColor(
+                item.status.isInStock ? nil : Color.gray
+              )
+            }
+          }
+        }
     }
 }
 
-//struct InventoryView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        InventoryView()
-//    }
-//}
+struct Inventory_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            InventoryView(
+                store: Store(
+                    initialState: InventoryFeature.State(
+                        items: [
+                            .headphones,
+                            .mouse,
+                            .keyboard,
+                            .monitor,
+                        ]
+                    ),
+                    reducer: InventoryFeature()
+                )
+            )
+        }
+    }
+}
